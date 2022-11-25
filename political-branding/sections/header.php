ad81f7ec192f027e5acbbafaaf8cec4388992d3c
md-ameen <mdameenbf@gmail.com>
2022-11-25 12:13:10 +0530

    header section added in political branding

---
+++ political-branding/sections/header.php
@@ -0,0 +1,25 @@
+<link rel="stylesheet" href="assets/styles/style-header.css">
+
+<section id="header-section">
+    <div class="container">
+        <div class="landing-height d-flex align-items-center">
+            <div class="row service-row justify-content-center align-items-end">
+                <div class="col-md-6">
+                    <h2 class="service-heading">
+                        political branding
+                    </h2>
+                    <p class="service-content">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam sit amet vestibulum enim. Aliquam venenatis commodo vehicula. Nullam aliquet faucibus ipsum eu porttitor.
+                    </p>
+                    <button class="btn btn-danger more-btn">
+                        more information
+                    </button>
+                </div>
+                <div class="col-md-6 mt-5 mt-md-0 icon-col text-center">
+                    <img src="assets/images/hand-pointer.svg" alt="" class="img-fluid pointer">
+                    <img src="assets/images/ink.svg" alt="" class="img-fluid ink">
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
